79c1b1f06cf3bdbc24d55b2fb300062fb946b23d
eduard <eduardbordukov3@gmail.com>
2024-04-04 10:41:49 +0500

    The main app is ready, pages index and about

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-*ttxjcjfoxwc@66zdk=5%$dq^g+s6djj!rcqgd#tjndu2phrn_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -117,6 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+STATICFILES_DIRS = [
+   BASE_DIR / "static"
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
